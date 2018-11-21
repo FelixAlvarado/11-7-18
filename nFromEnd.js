4723e21f162467069569fff99fe37345c914b989
FelixAlvarado <felixalvarado653@gmail.com>
2018-11-21 11:15:20 -0800

    did more efficient nth from node solution

--- nFromEnd.js
+++ nFromEnd.js
@@ -18,3 +18,36 @@ var removeNthFromEnd = function(head, n) {
     return head;
     
 };
+// Given a linked list, remove the n-th node from the end of list and return its head.
+
+// Example:
+
+// Given linked list: 1->2->3->4->5, and n = 2.
+
+// After removing the second node from the end, the linked list becomes 1->2->3->5.
+//faster solution to problem
+
+ function ListNode(val) {
+    this.val = val;
+     this.next = null;
+ }
+var removeNthFromEnd = function(head, n) {
+    let start = new ListNode(0);
+    let slow = start;
+    let fast = start;
+    slow.next = head;
+
+    //Move fast in front so that the gap between slow and fast becomes n
+    for(let i=1; i<=n+1; i++)   {
+        fast = fast.next;
+    }
+    //Move fast to the end, maintaining the gap
+    while(fast != null) {
+        slow = slow.next;
+        fast = fast.next;
+    }
+    //Skip the desired node
+    slow.next = slow.next.next;
+    return start.next;
+    
+};
